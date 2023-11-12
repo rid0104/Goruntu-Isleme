aba2b4762fc6a0b2f57b199ed5f7b4390426a7b7
Rabia İrem Dişçeken <92097174+rid0104@users.noreply.github.com>
2023-11-12 18:56:45 +0300

    Add files via upload

---
+++ ödev1.py
@@ -0,0 +1,37 @@
+
+import cv2
+
+import numpy as np
+from matplotlib import pyplot as plt
+renkli=cv2.imread("landscape.jpg")
+
+
+gri=cv2.imread("landscape.jpg",0)
+
+cv2.imshow("gri manzara",gri)
+
+cv2.waitKey(0)
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Görüntüyü yükle
+img = cv2.imread('landscape.jpg', cv2.IMREAD_GRAYSCALE)
+
+histogram = np.zeros(256)
+
+for i in range(img.shape[0]):
+    for j in range(img.shape[1]):
+        pixel = img[i,j]
+        histogram[pixel] += 1
+
+
+plt.figure()
+plt.title(" Manzara Fotografının Gri  Histogramı")
+plt.xlabel("Piksel Değerleri")
+plt.ylabel("Frekans")
+
+plt.plot(histogram)
+
+plt.show()
