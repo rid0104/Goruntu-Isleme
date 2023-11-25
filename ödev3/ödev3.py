44be22734fc3aba3f7f0e1cd46f311d0d4329c20
Rabia İrem Dişçeken <92097174+rid0104@users.noreply.github.com>
2023-11-26 01:51:18 +0300

    ödev3

---
+++ ödev3/ödev3.py
@@ -0,0 +1,24 @@
+import cv2
+import n
+
+
+gri=cv2.imread("pirinc2.jpg",0)
+cv2.imshow("gri pirinc2",gri)
+
+thresh= 155
+ret,thresh_gri=cv2.threshold(gri,thresh,255,cv2.THRESH_BINARY)
+
+cv2.imshow("thresh gri",thresh_gri)
+
+contours, hierarchy = cv2.findContours(thresh_gri, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+print("Bulunan nesne sayısı: ", len(contours))
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
+
